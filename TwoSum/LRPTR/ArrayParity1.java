246e0954808dc537543b272ad06083db573694e4
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-29 22:04:25 +0530

    Two Sum

---
+++ TwoSum/LRPTR/ArrayParity1.java
@@ -0,0 +1,28 @@
+package TwoSum.LRPTR;
+
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
+
+        while(left<right){
+
+            if(nums[left] % 2==0){
+                left++;
+            }else if(nums[right] % 2 == 1){
+                right--;
+            }else{
+                
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+
+            }         
+
+          
+        }
+
+        return nums;
+        
+    }
+}
